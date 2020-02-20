1a8c13a8bf3dfac44149433a1c743ecadf476379
Piotr Ptak <piotr.a.ptak@capgemini.com>
2020-02-20 15:33:17 +0100

    Add configuration information for vault connection

---
+++ src/FakeWebApi/Configuration/Vault/VaultConnectionConfiguration.cs
@@ -0,0 +1,15 @@
+namespace FakeWebApi.Configuration.Vault
+{
+        public class VaultConnectionConfiguration
+    {
+        public string VaultAddress { get; set; }
+        public string AuthenticationToken { get; set; }
+        public CommonConnection Common { get; set; }
+    }
+
+    public class CommonConnection
+    {
+        public string EnginePath { get; set; }
+        public string ElasticSearchPath { get; set; }
+    }
+}
