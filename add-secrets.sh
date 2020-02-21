06f63cf17fe6671df5c43a9140ec88a8cb756f33
Piotr Ptak <piotr.a.ptak@capgemini.com>
2020-02-21 09:08:25 +0100

    add script for creating secrets in hashicorp vault

---
+++ add-secrets.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+export VAULT_ADDR='http://0.0.0.0:8200'
+
+vault login 00000000-0000-0000-0000-000000000000
+
+vault secrets enable -version=2 -path=Common kv
+
+vault kv put Common/Elasticsearch Url=http://localhost:9200/
