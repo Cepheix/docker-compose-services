f4c0be10cd59cf87cb6d7cf429f9f88afbe2cf10
Piotr Ptak <piotr.a.ptak@capgemini.com>
2020-03-25 12:11:19 +0100

    Add vault secret for monitoring

--- add-secrets.sh
+++ add-secrets.sh
@@ -7,3 +7,5 @@ vault login 00000000-0000-0000-0000-000000000000
 vault secrets enable -version=2 -path=Common kv
 
 vault kv put Common/Elasticsearch Url=http://localhost:9200/
+
+vault kv put Common/Metrics Url=http://localhost:9091/metrics
